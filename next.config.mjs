c2b0eb4613663b790e78fdeeb66e52f2d0486996
Lucas Balduino <lalussac@gmail.com>
2024-11-09 17:17:36 -0300

    configss3

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 import { withContentlayer } from "next-contentlayer";
+import { NextConfig } from 'next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
+	basepath: '/app',
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 	experimental: {
 		mdxRs: true,

73b397d67a9c7c0d50416d111f4c4be3b674fd7f
Lucas Balduino <lalussac@gmail.com>
2024-11-09 17:28:18 -0300

    espero ser o ultimo

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 import { withContentlayer } from "next-contentlayer";
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
-	basepath: '/app',
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+  output: 'export',
+  basePath: '/app', 
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
 };
 
 export default withContentlayer(nextConfig);
-
